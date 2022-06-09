497d2be4329df5bd1c9a35490e02f4f16694625a
Kariuki1976 <emmanuel.mwangi@student.moringaschool.com>
2022-06-09 14:19:31 +0300

    create an aboutus page

--- app/src/main/java/com/example/phonetracker/aboutus.java
+++ app/src/main/java/com/example/phonetracker/aboutus.java
@@ -1,2 +1,15 @@
-package com.example.phonetracker;public class aboutus {
+package com.example.phonetracker;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aboutus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+    }
 }
