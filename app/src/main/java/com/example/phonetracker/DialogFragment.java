21a7f19997921c0a78b2486e86dda1e1cad67699
Kariuki1976 <emmanuel.mwangi@student.moringaschool.com>
2022-06-09 14:22:15 +0300

    create sales/customer dialog

--- app/src/main/java/com/example/phonetracker/DialogFragment.java
+++ app/src/main/java/com/example/phonetracker/DialogFragment.java
@@ -1,2 +1,36 @@
-package com.example.phonetracker;public class DialogFragment {
+package com.example.phonetracker;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import java.util.Objects;
+
+public class DialogFragment extends androidx.fragment.app.DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(getActivity()));
+        builder.setTitle("Dialog via Builder");
+        builder.setMessage("Would you like to see our Phone?");
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });
+
+        builder.setNegativeButton("Nope", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });
+        return builder.create();
+    }
 }
