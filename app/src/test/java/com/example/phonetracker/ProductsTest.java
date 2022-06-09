1d817de6622aee869600fbe51eb53d3a87a98662
Kariuki1976 <emmanuel.mwangi@student.moringaschool.com>
2022-06-09 14:25:53 +0300

    test for product

---
+++ app/src/test/java/com/example/phonetracker/ProductsTest.java
@@ -0,0 +1,43 @@
+package com.example.phonetracker;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class ProductsTest {
+    private Products products;
+
+    @Before
+    public void setUp(){
+        products = Robolectric.buildActivity(Products.class)
+                .create()
+                .start()
+                .resume()
+                .get();
+
+    }
+    @Test
+    public  void userInput(){
+        EditText editText = products.findViewById(R.id.editTextTextPersonName);
+        assertEquals("name", editText.getHint());
+
+    }
+    @Test
+    public void validateButton(){
+        ImageButton button = products.findViewById(R.id.contactUs);
+        assertNotNull(button);
+
+    }
+}
