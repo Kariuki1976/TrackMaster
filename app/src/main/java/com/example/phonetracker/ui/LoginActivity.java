236fac7935c27dd92aa0770c66a225a509159bba
Kariuki1976 <emmanuel.mwangi@student.moringaschool.com>
2022-06-18 08:37:59 +0300

    add progress bar

--- app/src/main/java/com/example/phonetracker/ui/LoginActivity.java
+++ app/src/main/java/com/example/phonetracker/ui/LoginActivity.java
@@ -71,9 +71,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
+                    Intent inttent = new Intent(LoginActivity.this, MainActivity.class);
+                    inttent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(inttent);
                     finish();
                 }
             }
